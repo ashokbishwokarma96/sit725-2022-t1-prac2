0ac6d96adf14ba17b13f22f0e4f1753ec4583dae
ashokbishwokarma96 <ashokbishwokarma96@gmail.com>
2022-08-02 15:02:52 +1000

    get method to add two numbers

--- server.js
+++ server.js
@@ -5,3 +5,16 @@ var port = process.env.port || 3000;
 app.listen(port, () => {
   console.log(`App listening to:${port}`);
 });
+
+const addNumbers = (num1, num2) => {
+  var numberOne = parseInt(num1);
+  var numberTwo = parseInt(num2);
+  var result = numberOne + numberTwo;
+  return result;
+};
+app.get("/addTwoNumbers", (req, res) => {
+  var numOne = req.query.numOne;
+  var numTwo = req.query.numTwo;
+  var result = addNumbers(numOne, numTwo);
+  res.json({ statusCode: 200, data: result, message: "success" });
+});
